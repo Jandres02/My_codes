5795e1e176161c03c2b534112096d12be387157c
Jandres02 <avjohan@uninorte.edu.co>
2022-05-04 12:21:25 -0500

    04/05/2022

--- flask_server/app.py
+++ flask_server/app.py
@@ -6,7 +6,7 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port= 1202)
 
 @app.route('/hello/<name>')
 def hello(name):
